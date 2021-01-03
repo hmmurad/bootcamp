c45ca2acf24d1b35e0eb335368181069a7bae294
hmmurad <murad91joy@gmail.com>
2021-01-03 23:42:48 +0600

    This is third commit to add Add Product page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'  },
   { path: 'home', component: HomeComponent  },
   { path: 'about', component: AboutComponent  },
-  { path: 'add-Product', component: AddProductComponent  },
+  { path: 'add-product', component: AddProductComponent  },
   { path: '**', component: PageNotFoundComponent  },
 ];
 
